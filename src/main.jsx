8c89ad972c85191fadba462b0170a6555935774e
Pedro Andriola <riqueft@icloud.com>
2025-07-30 16:40:30 -0300

    com instrumentacao

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,21 @@
+import { datadogRum } from '@datadog/browser-rum';
+import { reactPlugin } from '@datadog/browser-rum-react';
+
+datadogRum.init({
+    applicationId: '37c4b5d3-3c7f-4568-a248-6434c333a7a4',
+    clientToken: 'pub057f0b0205e4167931819850d9294551',
+    site: 'datadoghq.com',
+    service:'frontend',
+    env: 'prod',
+    
+    // Specify a version number to identify the deployed version of your application in Datadog
+    // version: '1.0.0',
+    sessionSampleRate:  100,
+    sessionReplaySampleRate: 100,
+    defaultPrivacyLevel: 'allow',
+    plugins: [reactPlugin({ router: true })],
+});
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
